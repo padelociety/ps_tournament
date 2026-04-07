eb2ed5fba586247ab8263de955bd2f3e1759782d
padelociety <padelociety@gmail.com>
2026-04-07 18:25:48 +0900

    Add files via upload

---
+++ tournament_sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "ps-tournament-v1";
+const ASSETS = [
+  "./tournament.html",
+  "./tournament-app.jsx",
+  "./tournament_manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "https://fonts.googleapis.com/css2?family=Pretendard:wght@400;500;600;700;800;900&display=swap",
+  "https://unpkg.com/react@18/umd/react.production.min.js",
+  "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
+  "https://unpkg.com/@babel/standalone/babel.min.js",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
